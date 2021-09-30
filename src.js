a61e9ba218eb3d6a80d2d3193e68d504662c4be1
IgorKuzmich <igorkuzmich96@gmail.com>
2021-09-30 03:27:05 -0400

    added front end

--- src.js
+++ src.js
@@ -1,3 +1,78 @@
+const result = document.querySelector('#result');
+const playerChoice = document.querySelector('#playerChoice');
+const computerChoice = document.querySelector('#computerChoice');
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
+const buttons = document.querySelector('#buttons');
+
+const choices = document.querySelectorAll('.choice');
+choices.forEach(c => c.addEventListener('click', playGame));
+
+const resetButton = document.createElement('button');
+resetButton.classList.add('choice');
+resetButton.addEventListener('click', reset);
+const resetImage = document.createElement('img');
+resetImage.src = 'images/reset.svg';
+resetButton.appendChild(resetImage);
+
+let pScore = 0;
+let cScore = 0;
+
+function playGame(e) {
+    const playerSelection = e.currentTarget.id;
+    const computerSelection = computerPlay();
+    const outcome = playRound(playerSelection, computerSelection);
+
+    playerChoice.src = `images/${playerSelection}.svg`
+    computerChoice.src = `images/${computerSelection}.svg`
+    
+    if (outcome === -1) {
+        result.textContent = "You Lose!";
+        cScore++;
+    } else if (outcome === 0) {
+        result.textContent = "Tie!";
+    } else if (outcome === 1) {
+       result.textContent = "You Win!";
+       pScore++;
+    }
+    updateScore(pScore, cScore);
+    if (pScore == 5) {
+        endGame(true);
+    } else if (cScore == 5) {
+        endGame(false);
+    }
+}
+
+function endGame(isPlayerWinner) {
+    if (isPlayerWinner) {
+        result.textContent = "Congratulations!!! You Won!!!";
+    }
+    else {
+        result.textContent = "Sorry! You Lost!";
+    }
+
+    choices.forEach(c => buttons.removeChild(c));
+    buttons.appendChild(resetButton);
+}
+
+function reset() {
+    pScore = 0;
+    cScore = 0;
+    updateScore(pScore, cScore);
+
+    playerChoice.src = "images/rock.svg";
+    computerChoice.src = "images/rock.svg";
+    result.textContent = "Let's Play!";
+
+    buttons.removeChild(resetButton);
+    choices.forEach(c => buttons.appendChild(c));
+}
+
+function updateScore(pScore, cScore) {
+    computerScore.textContent = `${cScore}`;
+    playerScore.textContent = `${pScore}`;
+}
+
 function computerPlay() {
     const play = ["rock", "paper", "scissors"]
     let choice = Math.floor(Math.random() * 3)
@@ -21,25 +96,3 @@ function playRound(playerSelection, computerSelection) {
 
     return -1
 }
-
-function game() {
-    let score = 0
-    for (let round = 0; round < 5; round++) {
-        playerSelection = window.prompt("Choose: Rock, Paper, or Scissors")
-        computerSelection = computerPlay()
-        result = playRound(playerSelection, computerSelection)
-
-        if (result === -1) {
-            console.log("Lose: " + computerSelection + " beats " + playerSelection)
-            score--
-        } else if (result === 0) {
-            console.log("Tie: both chose " + computerSelection)
-        } else if (result === 1) {
-            console.log("Win: " + playerSelection + " beats " + computerSelection)
-            score++
-        }
-        console.log("Score: " + score)
-    }
-}
-
-game()
